0050b783a118a73448406aa911d14d45bceeb8c2
qitian <qitia@192.168.2.16>
2020-09-03 13:17:32 -0400

    join component and chat component were created

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Join from './components/Join';
+import Chat from './components/Chat';
+
+const App = () => (
+    <Router>
+        <Route path = "/" exact component = {Join} />
+        <Route path = "/chat" component = {Chat} />
+    </Router>
+);
+
+export default App;
+/*
+User will be greeted with the join component when he first visits the page. 
+Then he enters the username and password to log into his account.
+After this user will meet the chat component.
+*/
---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App'
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+// everything created in react is going to be inside of the App and put it inside of the root
+
+
