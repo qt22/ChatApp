fe5a87b8c58c30c2cf2c48a43e9f896dca047791
qitian <qitia@192.168.2.16>
2020-09-12 23:42:10 -0400

    users joining and disconnecting completed

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search); // retrieve the data that users entered
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room); 
+
+        socket.emit('join', { name, room }, () => {
+            
+        }); // send data to back end
+
+        return () => {
+            socket.emit('disconnect');
+
+            socket.off();
+        }
+            // disconnection
+    }, [ENDPOINT, location.search]);
     return (
         <h1>Chat</h1>
     )
